1fb5c805865f83bca0807a28a08c332a84c3fa66
Peter Pult <peter.pult@netlight.com>
2020-01-11 14:01:58 +0100

    started implementing authentication

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const clientId = process.env.SLACK_CLIENT_ID;
+const clientSecret = process.env.SLACK_CLIENT_SECRET;
+const verification = process.env.SLACK_VERIFICATION_TOKEN;
+
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Hello Dashlight!'));
+
+app.get('/auth'), (req, res) => {};
+
+app.post('/events', (req, res) => {
+  const { type, challenge } = req.body;
+
+  if (type === 'url_verification') {
+    return res.json({ challenge });
+  }
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
